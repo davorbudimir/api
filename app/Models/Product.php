f02ba4f889aa123458a97e1ffd50e34112a6bb02
Davor Budimir <dbudimir8@gmail.com>
2019-03-02 21:18:01 +0100

    Relationship created

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $table = 'products';
+
+    public function reviews(){
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    protected $table = 'reviews';
+
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
 }
